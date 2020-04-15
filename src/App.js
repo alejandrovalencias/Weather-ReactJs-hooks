e1853f9e070853a8ba10f62d0ae48049af865f82
Alejandro <alejandrov.net@gmail.com>
2020-04-15 10:58:35 -0500

    Se consulta api the weather para traer el clima de una ciudad

--- src/App.js
+++ src/App.js
@@ -1,53 +1,67 @@
-import React,{useState} from 'react';
-import Header from './components/Header';
-import Formulario from './components/Formulario';
-import Error from './components/Error';
-
+import React, { useState, useEffect } from "react";
+import Header from "./components/Header";
+import Formulario from "./components/Formulario";
+import Error from "./components/Error";
+import Clima from "./components/Clima";
 
 function App() {
+  const [ciudad, setCiudad] = useState("");
+  const [pais, setPais] = useState("");
+  const [error, setError] = useState(false);
+  const [resultado, setResultado] = useState({});
 
-    const [ciudad, setCiudad] = useState('');
-    const [pais, setPais] = useState('');
-    const [error, setError] = useState(false);
+  useEffect(() => {
+    if (ciudad === "") return;
 
-    const datosConsulta = (datos) => {
-        if(datos.ciudad === '' || datos.pais === ''){
-            //Error
-            setError(true);
-            return;
-        }
-        //Se agregan al state
-        setCiudad(datos.ciudad);
-        setPais(datos.pais);
-        setError(false);
+    consultarApi();
+  }, [ciudad, pais]);
+
+  const datosConsulta = (datos) => {
+    if (datos.ciudad === "" || datos.pais === "") {
+      setError(true);
+      return;
     }
-    
-    //Cargar un componente condicionalmente
-    let componente = null;
-    if(error === true){
-        componente = <Error mensaje="Debe seleccionar ciudad y país"/>;        
-    }
+    //Se agregan al state
+    setCiudad(datos.ciudad);
+    setPais(datos.pais);
+    setError(false);
+  };
 
-    return(
-        <div className="App">
-            <Header titulo="Clima react"/>
+  const consultarApi = async () => {
+    const appId = `5050e931fb63ed263c67e1293bceeb49`;
+    const urlApi = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
 
-            <div className="contenedor-form">
-                <div className="container">
-                    <div className="row">
-                        <div className="col s12 md6">
-                            <Formulario
-                            datosConsulta={datosConsulta}
-                            />
-                        </div>
-                        <div className="col s12 md6">
-                          {componente}
-                        </div>
-                    </div>
-                </div>
+    //Se consume la api del clima
+    const respuesta = await fetch(urlApi);
+    const resultado = await respuesta.json();
+    setResultado(resultado);
+  };
+
+  //Cargar un componente condicionalmente
+  let componente = null;
+  if (error === true) {
+    componente = <Error mensaje="Debe seleccionar ciudad y país" />;
+  } else if (resultado.cod === "404") {
+    componente = <Error mensaje="Ciudad no encontrada" />;
+  } else {
+    componente = <Clima resultado={resultado} />;
+  }
+
+  return (
+    <div className="App">
+      <Header titulo="Weather in ReactJs" />
+      <div className="contenedor-form">
+        <div className="container">
+          <div className="row">
+            <div className="col s12 md6">
+              <Formulario datosConsulta={datosConsulta} />
             </div>
+            <div className="col s12 md6">{componente}</div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,53 +1,50 @@
-import React,{useState} from 'react';
+import React, { useState } from "react";
 
-const Formulario = ({datosConsulta}) => {
-    //State del componente
-    //busqueda = state guardarBusqueda = this.setState({})
-    const [busqueda, setBusqueda] = useState({ciudad:'',pais:''});
+const Formulario = ({ datosConsulta }) => {
+  //State del componente
+  //busqueda = state guardarBusqueda = this.setState({})
+  const [busqueda, setBusqueda] = useState({ ciudad: "", pais: "" });
 
-    const handleChange = e =>{
-        setBusqueda({
-            ...busqueda,
-            [e.target.name] : e.target.value
-        });
+  const handleChange = (e) => {
+    setBusqueda({
+      ...busqueda,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-        console.log(busqueda);
-    }
+  const consultaClima = (e) => {
+    e.preventDefault();
 
-    const consultaClima = e =>{
-        e.preventDefault();
+    //Pasar hacia el componente principal la busqueda
+    datosConsulta(busqueda);
+  };
 
-        //Pasar hacia el componente principal la busqueda
-        datosConsulta(busqueda);
-    }
-
-return(
+  return (
     <form onSubmit={consultaClima}>
-        <div className="input-field col s12">
-          <input
-            type="text"
-            name="ciudad"
-            id="ciudad"
-            onChange={handleChange}
-            />
-            <label htmlFor="ciudad">Ciudad: </label>
-        </div>
-        
-        <div className="input-field col s12">
-            <select onChange={handleChange} name="pais">
-                <option value="">Seleccione el país</option>
-                <option value="CO">Colombia</option>
-                <option value="DN">Dinamarca</option>
-                <option value="AR">Argentina</option>
-                <option value="US">Estados unidos</option>
-            </select>
-        </div>
+      <div className="input-field col s12">
+        <input type="text" name="ciudad" id="ciudad" onChange={handleChange} />
+        <label htmlFor="ciudad">Ciudad: </label>
+      </div>
 
-        <div className="input-field col s12">
-            <input type="submit" className="waves-effect waves-light btn-large btn-block yellow  accent-4" value="Consulta clima"/>
-        </div>
+      <div className="input-field col s12">
+        <select onChange={handleChange} name="pais">
+          <option value="">Seleccione el país</option>
+          <option value="CO">Colombia</option>
+          <option value="DN">Dinamarca</option>
+          <option value="AR">Argentina</option>
+          <option value="US">Estados unidos</option>
+        </select>
+      </div>
+
+      <div className="input-field col s12">
+        <input
+          type="submit"
+          className="waves-effect waves-light btn-large btn-block yellow  accent-4"
+          value="Consulta clima"
+        />
+      </div>
     </form>
-);
-}
+  );
+};
 
 export default Formulario;
